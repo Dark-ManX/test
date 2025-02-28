102751de62a95b58c0d7a0b41b50ed0a35e05c86
Serhii <sergiosis@ukr.net>
2025-02-28 09:04:19 +0200

    fix

---
+++ src/app/(main)/job-details/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { Modal } from "@/app/(main)/jobs/(..)job-details/[id]/modal";
+import { JobDetails } from "@/features/job-details";
+
+const Page = () => {
+  return (
+    <Modal>
+      <JobDetails />
+    </Modal>
+  );
+};
+
+export default Page;
